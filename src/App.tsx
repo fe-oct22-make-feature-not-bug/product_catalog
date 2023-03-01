a6f4ed7a8b118aaef20e5902de0809699cacab57
Olha Mamai <olgga.ju@gmail.com>
2023-03-01 16:23:40 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,21 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import './styles/index.scss'
-import { Footer } from './components/Footer/Footer';
-import { PageNotFound } from './components/PageNotFound/PageNotFound';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import "./styles/index.scss";
+import { Footer } from "./components/Footer/Footer";
+import { PageNotFound } from "./components/PageNotFound/PageNotFound";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route
-          path="/"
-          element={<h1>Home page!</h1>}
-        /> 
+        <Route path="/" element={<h1>Home page!</h1>} />
 
-        <Route
-          path="/phones"
-          element={<h1>Mobile phones!</h1>}
-        />
+        <Route path="/phones" element={<h1>Mobile phones!</h1>} />
 
-        <Route
-          path="*"
-
-          element={< PageNotFound />}
-        />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
 
-     < Footer />
+      <Footer />
     </div>
   );
 }
