69af159851c5dd27a778dec0643de283ad9b57af
TetKopot <tet.kopot@gmail.com>
2023-03-02 17:31:19 +0200

    header is added

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,12 @@ import "./styles/index.scss";
 import { Footer } from "./components/Footer/Footer";
 import { PageNotFound } from "./components/PageNotFound/PageNotFound";
 import { Card } from "./components/Card/Card";
+import { Header } from "./components/Header/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Routes>
         <Route path="/" element={<h1>Home page!</h1>} />
 
