c9c410c1942fe02dd9d19a96d87058a9b4beb74a
TetKopot <tet.kopot@gmail.com>
2023-03-02 22:33:12 +0200

    use typography

--- src/components/Header/BurgerMenu/BurgerMenu.tsx
+++ src/components/Header/BurgerMenu/BurgerMenu.tsx
@@ -1,21 +1,15 @@
-import React from 'react';
-import './BurgerMenu.scss';
+import React from "react";
+import "./BurgerMenu.scss";
 // import { Logo } from '../../Logo';
 // import like from '../../../images/like.png';
 // import bag from '../../../images/bag.png';
 // import menu from '../../../images/menu.png';
 
-
 export const BurgerMenu: React.FC = () => {
   return (
     <div className="menu" id="menu">
       <div className="menu__header">
-        <a 
-          href="#home"
-          className='header__logo'
-        >
-          
-        </a>
+        {/* <a href="#home" className="header__logo"></a> */}
       </div>
     </div>
   );
