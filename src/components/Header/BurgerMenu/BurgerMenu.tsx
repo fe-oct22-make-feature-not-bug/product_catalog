5bcb43b88a8ca047b07e8774db49537720b5928b
TetKopot <tet.kopot@gmail.com>
2023-03-06 20:15:15 +0200

    add burger menu

--- src/components/Header/BurgerMenu/BurgerMenu.tsx
+++ src/components/Header/BurgerMenu/BurgerMenu.tsx
@@ -1,16 +1,102 @@
-import React from "react";
+import React, { Dispatch, SetStateAction, memo } from "react";
+import cn from "classnames";
+import { NavLink } from "react-router-dom";
 import "./BurgerMenu.scss";
-// import { Logo } from '../../Logo';
-// import like from '../../../images/like.png';
-// import bag from '../../../images/bag.png';
-// import menu from '../../../images/menu.png';
+import like from "../../../assets/icons/like.png";
+import bag from "../../../assets/icons/bag.png";
 
-export const BurgerMenu: React.FC = () => {
-  return (
-    <div className="menu" id="menu">
-      <div className="menu__header">
-        {/* <a href="#home" className="header__logo"></a> */}
-      </div>
-    </div>
-  );
+type Props = {
+  burgerMenuActive: boolean;
+  setBurgerMenuActive: Dispatch<SetStateAction<boolean>>;
 };
+
+export const BurgerMenu: React.FC<Props> = memo(
+  ({ burgerMenuActive, setBurgerMenuActive }) => {
+    return (
+      <div className={cn("menu", { active: burgerMenuActive })}>
+        <div className="menu__header">
+          <button
+            type="button"
+            className="menu__close"
+            onClick={() => setBurgerMenuActive(false)}
+          >
+            {true}
+          </button>
+        </div>
+
+        <nav className="menu__nav">
+          <ul className="menu__list">
+            <li className="menu__item text-uppercase">
+              <NavLink
+                to="/"
+                end
+                className={({ isActive }) =>
+                  isActive
+                    ? "menu__link menu__link--is--active is-active"
+                    : "menu__link"
+                }
+                onClick={() => setBurgerMenuActive(false)}
+              >
+                home
+              </NavLink>
+            </li>
+
+            <li className="menu__item text-uppercase">
+              <NavLink
+                to="/phones"
+                className={({ isActive }) =>
+                  isActive
+                    ? "menu__link nav__link--is--active is-active"
+                    : "menu__link"
+                }
+                onClick={() => setBurgerMenuActive(false)}
+              >
+                phones
+              </NavLink>
+            </li>
+            <li className="menu__item text-uppercase">
+              <NavLink
+                to="/tablets"
+                className={({ isActive }) =>
+                  isActive
+                    ? "menu__link nav__link--is--active is-active"
+                    : "menu__link"
+                }
+                onClick={() => setBurgerMenuActive(false)}
+              >
+                tablets
+              </NavLink>
+            </li>
+            <li className="menu__item text-uppercase">
+              <NavLink
+                to="/accessories"
+                className={({ isActive }) =>
+                  isActive
+                    ? "menu__link menu__link--is--active is-active"
+                    : "menu__link"
+                }
+                onClick={() => setBurgerMenuActive(false)}
+              >
+                accessories
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+
+        <div className="menu__likeAndBag">
+          <div className="like">
+            <NavLink to="/like" onClick={() => setBurgerMenuActive(false)}>
+              <img src={like} alt="like" className="image" />
+            </NavLink>
+          </div>
+
+          <div className="bag">
+            <NavLink to="/bag" onClick={() => setBurgerMenuActive(false)}>
+              <img src={bag} alt="bag" className="image" />
+            </NavLink>
+          </div>
+        </div>
+      </div>
+    );
+  }
+);
