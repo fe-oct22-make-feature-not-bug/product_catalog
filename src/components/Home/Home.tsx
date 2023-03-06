b55d639e3b962953fe682e5567f399fdb82b328a
Kris Planka <kristyaplanka@gmail.com>
2023-03-06 16:22:35 +0200

    fixed slick & wish-btn

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,11 +1,14 @@
 import React, { memo } from "react";
 import { Banner } from "../Banner/Banner";
+import { Card } from "../Card/Card";
 import "./Home.scss";
 
 export const Home: React.FC = memo(() => {
   return (
     <main className="main">
+      <h1 className="h1 home-header">Welcome to Nice Gadgets store!</h1>
       <Banner />
+      <Card />
     </main>
   );
 });
