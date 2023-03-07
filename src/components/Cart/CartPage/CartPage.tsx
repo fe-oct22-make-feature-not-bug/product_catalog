b9359e59805bbbe854dcf4a9ebf2922f99c27a16
Olha Mamai <olgga.ju@gmail.com>
2023-03-07 18:32:16 +0200

    fixed flex

--- src/components/Cart/CartPage/CartPage.tsx
+++ src/components/Cart/CartPage/CartPage.tsx
@@ -3,14 +3,16 @@ import { CartTotal } from "../CartTotal";
 import './CartPage.scss';
 
 export const CartPage: React.FC = () => {
-  return(
+  return (
     <section className="cart">
       <h1 className="cart__title">Cart</h1>
-      <div className="container">
-      <div className="cart__items">
-      <CartItem/>
-      </div>
-      <CartTotal/>
+
+      <div className="cart__container">
+        <div className="cart__items">
+          <CartItem />
+        </div>
+
+        <CartTotal />
       </div>
     </section>
   );
